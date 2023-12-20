9efc72fbdd4bfde880030a72b62e87f6f2ab2798
asamoah953 <asamoahclinton953@gmail.com>
2023-12-19 16:24:23 -0800

    I am federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,36 @@
 #include "main.h"
-
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
- * Return: ...
+ * jack_bauer - print every minute of the day, starting from 00:00 - 23:59.
+ *
+ * Return : void
  */
-
 void jack_bauer(void)
-        
 {
-        
-       
-        int a, b, c, d;
-         
-        for (a = 0; a<= 2; a++)
-        {
-        for (b=0; b <= 9; b++)
+	int h0, h1;
+	int m0, m1;
+
+	for (h0 = 0 ; h0 <= 2 ; h0++)
 	{
-        if ((a <= 1 && b <=9) || (a <= 2 && b <= 3))
-	{
-        for (c =0; c <=5; c++)
-        {
-        for (d = 0; d <= 9; d++)
-        {
-        _putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
+		for (h1 = 0 ; h1 < 10 ; h1++)
+		{
+			for (m0 = 0 ; m0 < 6 ; m0++)
+			{
+				for (m1 = 0 ; m1 < 10 ; m1++)
+				{
+					_putchar('0' + h0);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m0);
+					_putchar('0' + m1);
+					_putchar('\n');
+					if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+					{
+						return;
+					}
+				}
+			}
+		}
 	}
+}
 
 
