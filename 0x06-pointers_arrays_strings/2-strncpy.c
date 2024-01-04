9c14451a20e42a0953d3f1e98c4000efc3320cc6
asamoah953 <asamoahclinton953@gmail.com>
2024-01-04 08:18:07 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,46 +1,27 @@
 #include "main.h"
 /**
- * print_number - print an int numbers.
- * @n: number tested
- * Return: Always 0.
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-void print_number(int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j, digit, digits, power;
-	unsigned int temp, numchar, number;
+	int j;
 
-	digit = 0;
-	if (n < 0)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		_putchar('-');
-		temp = -n;
+		dest[j] = src[j];
+		j++;
 	}
-	else
+	while (j < n)
 	{
-		temp = n;
+		dest[j] = '\0';
+		j++;
 	}
 
-	number = temp;
-
-	while (number >= 10)
-	{
-		number = number / 10;
-		digit++;
-	}
-	digits = digit + 1;
-	power = 1;
-	i = 1;
-
-	while (i < digits)
-	{
-		power = power * 10;
-		i++;
-	}
-	j = power;
-	while (j >= 1)
-	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + '0');
-		j = j / 10;
-	}
+	return (dest);
 }
