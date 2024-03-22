313078adb7f6893144802a53eec264c9ec1129cf
asamoah953 <asamoahclinton953@gmail.com>
2024-03-22 06:22:04 -0700

    6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
+
