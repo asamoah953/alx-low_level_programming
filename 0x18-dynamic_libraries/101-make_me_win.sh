c497437544f9ab5d1a6aad2327f7e2e3e3a309b6
asamoah953 <asamoahclinton953@gmail.com>
2024-03-29 18:36:31 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget .. -P https://raw.githubusercontent.com/asamoah953/alx-low_level_programming/master/0x18-dynamic_libraries
-export LD_PRELOAD="$PWD/../libhack.so"
+wget .. -P https://raw.githubusercontent.com/asamoah953/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
