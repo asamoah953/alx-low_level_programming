0a64392fcd0a88d3218f1d767c28f90016455644
asamoah953 <asamoahclinton953@gmail.com>
2024-07-20 04:31:03 -0700

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,21 +5,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-} listint_t;
 
 /**
  * struct listint_s - singly linked list
