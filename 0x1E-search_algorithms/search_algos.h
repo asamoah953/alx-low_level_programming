8f34f4ac742e753fbcaa6d4a5d6ba1fe9da4c67c
asamoah953 <asamoahclinton953@gmail.com>
2024-07-20 04:26:36 -0700

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,6 +21,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
